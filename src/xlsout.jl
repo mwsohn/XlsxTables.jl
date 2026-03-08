e40db929c1ebc374c2e9af96f37a0ee594b00375
mwsohn <mwsohn@gmail.com>
2026-03-08 03:29:54 -0400

    Update xlsout.jl

--- src/xlsout.jl
+++ src/xlsout.jl
@@ -608,7 +608,7 @@ function bivariatexls(df::AbstractDataFrame,
             # colvar levels
             for i = 1:nlev
                 sub = Dict(y.colnames .=> length(y.colnames))
-                if i <= size(y.omat, 1) && y.omat[i, sub[:n]] > 1
+                if i <= size(y.omat, 1) && y.omat[i, 1] > 1
                     LibXLSXWriter.worksheet_write_number(t,r, c + i * 2 + 1, y.omat[i, sub[:mean]], formats[:f_fmt_right])
                     LibXLSXWriter.worksheet_write_number(t,r, c + i * 2 + 2, y.omat[i, sub[:sd]], formats[:f_fmt_left_parens])
                 else
