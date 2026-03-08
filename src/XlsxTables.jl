0d88acac838e4f9aecd14f1354173a93744f6361
mwsohn <mwsohn@gmail.com>
2026-03-08 14:03:49 -0400

    Update XlsxTables.jl

--- src/XlsxTables.jl
+++ src/XlsxTables.jl
@@ -8,7 +8,7 @@ module XlsxTables
 
 using DataFrames, CategoricalArrays, Distributions, GLM, Survival, StatsBase, DataStructures,
     HypothesisTests, NamedArrays, FreqTables, Stella, TableMetadataTools, LogisticROC, 
-    Dates, OrderedCollections, AnalysisOfVariance, LibXLSXWriter
+    Dates, OrderedCollections, AnalysisOfVariance, LibXLSXWriter, Printf
     
 import LibXLSXWriter: workbook_new, workbook_add_worksheet, workbook_add_format, format_set_bold, worksheet_set_column, worksheet_write_string, worksheet_write_number, worksheet_insert_image, workbook_close
 
