4d713a10b10bc5f11b7a757bb3e545c6d9770861
mwsohn <mwsohn@gmail.com>
2026-03-10 00:13:50 -0400

    Update format_defs.jl

--- src/format_defs.jl
+++ src/format_defs.jl
@@ -1,396 +1,331 @@
 # -----------------------------------------------
 # format definitions
 #------------------------------------------------
-format_defs = Dict()
+format_defs = Dict(
 
-format_defs[:heading] = Dict(
-    "bold" => true,
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "center",
-    "border" => "thin"
-)
+    # format commands listed in the :global dictionary
+    # will be added to all the other dictionaries below
+    :global => Dict(
+        "font_name" => "Arial",
+        "font_size" => 9
+    ),
 
-format_defs[:text] = Dict(
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "border" => "thin"
-)
+    # header cells
+    :heading => Dict(
+        "bold" => true,
+        "valign" => "vcenter", 
+        "align" => "center", 
+        "border" => "thin"
+    ),
 
-format_defs[:heading_right] = Dict(
-    "bold" => true,
-    "font_name" => "Arial",
-    "font_size" => 10,
-    "valign" => "vcenter",
-    "align" => "right",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :heading_right => Dict(
+        "bold" => true,
+        "valign" => "vcenter",
+        "align" => "right",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ), 
+    
+    :heading_left => Dict(
+        "bold" => true,
+        "valign" => "vcenter",
+        "align" => "left",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:heading_left] = Dict(
-    "bold" => true,
-    "font_name" => "Arial",
-    "font_size" => 10,
-    "valign" => "vcenter",
-    "align" => "left",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    # string values
+    :text => Dict(
+        "valign" => "vcenter",
+        "align" => "left",
+        "border" => "thin"
+    ),
 
-format_defs[:model_name] = Dict(
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "border" => "thin"
-)
+    :model_name => Dict(
+        "valign" => "vcenter",
+        "align" => "left",
+        "border" => "thin"
+    ),
 
-format_defs[:varname_1indent] = Dict(
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "border" => "thin",
-    "indent" => 1
-)
+    :varname_1indent => Dict(
+        "valign" => "vcenter",
+        "align" => "left",
+        "border" => "thin",
+        "indent" => 1
+    ),
 
+    :varname_2indent => Dict(
+        "valign" => "vcenter",
+        "align" => "left",
+        "border" => "thin",
+        "indent" => 2
+    ),
 
-format_defs[:varname_2indent] = Dict(
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "border" => "thin",
-    "indent" => 2
-)
+    # numeric values
+    :n_fmt_right => Dict(
+        "num_format" => "#,##0",
+        "valign" => "vcenter",
+        "align" => "right",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
+    :n_fmt_right_noborder => Dict(
+        "num_format" => "#,##0",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "none"
+    ),
 
-format_defs[:n_fmt_right] = Dict(
-    "num_format" => "#,##0",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :n_fmt_left_parens => Dict(
+        "num_format" => "(#,##0)",
+        "valign" => "vcenter",
+        "align" => "right",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
+    :f_date => Dict(
+        "num_format" => "mm/dd/yyyy",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:n_fmt_left_parens] = Dict(
-    "num_format" => "(#,##0)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :f_datetime => Dict(
+        "num_format" => "mm/dd/yyyy hh:mm:ss",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:f_date] = Dict(
-    "num_format" => "mm/dd/yyyy",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    :f_fmt_right => Dict(
+        "num_format" => "#,##0.00",
+        "valign" => "vcenter",
+        "align" => "right",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:f_datetime] = Dict(
-    "num_format" => "mm/dd/yyyy hh:mm:ss",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    :f_fmt_center => Dict(
+        "num_format" => "#,##0.0000",
+        "valign" => "vcenter",
+        "align" => "center",
+        "border" => "thin"
+    ),
 
-format_defs[:f_fmt_right] = Dict(
-    "num_format" => "#,##0.00",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :f_fmt => Dict(
+        "num_format" => "#,##0.00",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:f_fmt_center] = Dict(
-    "num_format" => "#,##0.0000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "center",
-    "border" => "thin"
-)
+    :n_fmt => Dict(
+        "num_format" => "#,##0",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:f_fmt] = Dict(
-    "num_format" => "#,##0.00",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    :n_fmt_center => Dict(
+        "num_format" => "#,##0",
+        "valign" => "vcenter",
+        "align" => "center",
+        "border" => "thin"
+    ),
 
+    :f_fmt_left_parens => Dict(
+        "num_format" => "(#,##0.00)",
+        "valign" => "vcenter",
+        "align" => "left",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:n_fmt] = Dict(
-    "num_format" => "#,##0",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    :pct_fmt_parens => Dict(
+        "num_format" => "(0.00%)",
+        "valign" => "vcenter",
+        "align" => "left",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:n_fmt_center] = Dict(
-    "num_format" => "#,##0",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "center",
-    "border" => "thin"
-)
+    :pct_fmt => Dict(
+        "num_format" => "0.00%",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:f_fmt_left_parens] = Dict(
-    "num_format" => "(#,##0.00)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    # numeric values for odds ratios and their confidence intervals
+    :or_fmt => Dict(
+        "num_format" => "0.000",
+        "valign" => "vcenter",
+        "align" => "right",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
+    :cilb_fmt => Dict(
+        "num_format" => "(0.000\\,;(-0.000\\,",
+        "valign" => "vcenter",
+        "align" => "right",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:pct_fmt_parens] = Dict(
-    "num_format" => "(0.00%)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :ciub_fmt => Dict(
+        "num_format" => "0.000)",
+        "valign" => "vcenter",
+        "align" => "left",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
+    :or_fmt_red => Dict(
+        "num_format" => "0.000",
+        "font_color" => "red",
+        "valign" => "vcenter",
+        "align" => "right",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:pct_fmt] = Dict(
-    "num_format" => "0.00%",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    :cilb_fmt_red => Dict(
+        "num_format" => "(0.000\\,;(-0.000\\,)",
+        "font_color" => "red",
+        "valign" => "vcenter",
+        "align" => "right",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:or_fmt] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :ciub_fmt_red => Dict(
+        "num_format" => "0.000)",
+        "font_color" => "red",
+        "valign" => "vcenter",
+        "align" => "left",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:cilb_fmt] = Dict(
-    "num_format" => "(0.000\\,;(-0.000\\,",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :or_fmt_bold => Dict(
+        "num_format" => "0.000",
+        "bold" => true,
+        "valign" => "vcenter",
+        "align" => "right",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:ciub_fmt] = Dict(
-    "num_format" => "0.000)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "left",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :cilb_fmt_bold => Dict(
+        "num_format" => "(0.000 -",
+        "bold" => true,
+        "valign" => "vcenter",
+        "align" => "right",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
+    :ciub_fmt_bold => Dict(
+        "num_format" => "0.000)",
+        "bold" => true,
+        "valign" => "vcenter",
+        "align" => "left",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:or_fmt_red] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "font_color" => "red",
-    "valign" => "vcenter",
-    "align" => "right",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    # p-values
+    :p_fmt => Dict(
+        "num_format" => "0.000",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:cilb_fmt_red] = Dict(
-    "num_format" => "(0.000\\,;(-0.000\\,)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "font_color" => "red",
-    "valign" => "vcenter",
-    "align" => "right",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :p_fmt_center => Dict(
+        "num_format" => "0.000",
+        "valign" => "vcenter",
+        "align" => "center",
+        "border" => "thin"
+    ),
 
-format_defs[:ciub_fmt_red] = Dict(
-    "num_format" => "0.000)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "font_color" => "red",
-    "valign" => "vcenter",
-    "align" => "left",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :p_fmt2 => Dict(
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
+    :p_fmt_red => Dict(
+        "num_format" => "0.000",
+        "font_color" => "red",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:or_fmt_bold] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "bold" => true,
-    "valign" => "vcenter",
-    "align" => "right",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :p_fmt2_red => Dict(
+        "font_color" => "red",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:cilb_fmt_bold] = Dict(
-    "num_format" => "(0.000 -",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "bold" => true,
-    "valign" => "vcenter",
-    "align" => "right",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :p_fmt_bold => Dict(
+        "num_format" => "0.000",
+        "font_name" => "Arial",
+        "font_size" => 9,
+        "bold" => true,
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:ciub_fmt_bold] = Dict(
-    "num_format" => "0.000)",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "bold" => true,
-    "valign" => "vcenter",
-    "align" => "left",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
+    :p_fmt2_bold => Dict(
+        "bold" => "thin",
+        "valign" => "vcenter",
+        "align" => "right",
+        "border" => "thin"
+    ),
 
-format_defs[:p_fmt] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    # empty cells to take care of borders
+    :empty_border => Dict(
+        "valign" => "vcenter",
+        "border" => "thin"
+    ),
 
-format_defs[:p_fmt_center] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "center",
-    "border" => "thin"
-)
+    :empty_left => Dict(
+        "valign" => "vcenter",
+        "right" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-format_defs[:p_fmt2] = Dict(
-    #"num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
+    :empty_right => Dict(
+        "valign" => "vcenter",
+        "left" => "thin",
+        "bottom" => "thin",
+        "top" => "thin"
+    ),
 
-
-format_defs[:p_fmt_red] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "font_color" => "red",
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
-
-format_defs[:p_fmt2_red] = Dict(
-    #"num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "font_color" => "red",
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
-
-
-format_defs[:p_fmt_bold] = Dict(
-    "num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "bold" => true,
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
-
-format_defs[:p_fmt2_bold] = Dict(
-    #"num_format" => "0.000",
-    "font_name" => "Arial",
-    "font_size" => 9,
-    "bold" => "thin",
-    "valign" => "vcenter",
-    "align" => "right",
-    "border" => "thin"
-)
-
-format_defs[:empty_border] = Dict(
-    #"num_format" => "0.000",
-    "valign" => "vcenter",
-    "border" => "thin"
-)
-
-format_defs[:empty_left] = Dict(
-    #"num_format" => "0.000",
-    "valign" => "vcenter",
-    "right" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
-
-format_defs[:empty_right] = Dict(
-    #"num_format" => "0.000",
-    "valign" => "vcenter",
-    "left" => "thin",
-    "bottom" => "thin",
-    "top" => "thin"
-)
-
-
-format_defs[:empty_both] = Dict(
-    #"num_format" => "0.000",
-    "valign" => "vcenter",
-    "bottom" => "thin",
-    "top" => "thin"
+    :empty_both => Dict(
+        "valign" => "vcenter",
+        "bottom" => "thin",
+        "top" => "thin"
+    )
 )
