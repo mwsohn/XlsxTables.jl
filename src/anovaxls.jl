f7b4393fe35022db65f6555e1bb1b3bce38613fa
mwsohn <mwsohn@gmail.com>
2026-04-27 16:31:43 -0400

    Update anovaxls.jl

--- src/anovaxls.jl
+++ src/anovaxls.jl
@@ -54,9 +54,6 @@ function anovaxls(anov::ANOVA, wbook::Ptr, wsheet::String; row=0, col=0)
     for i in 1:dims[1] # rows
         for j in 1:dims[2] # columns
             v = mm[i, j]
-            # if ismissing(v)
-            #     continue # do not output data
-            # end
 
             if i == dims[1] # last row ofo "Total" - output with top and bottom borders
                 if j == 1 # source
@@ -73,18 +70,18 @@ function anovaxls(anov::ANOVA, wbook::Ptr, wsheet::String; row=0, col=0)
                     LibXLSXWriter.worksheet_write_string(t, c, r, string(v), formats[:source_name])
                 elseif j == 3 # DF
                     LibXLSXWriter.worksheet_write_number(t, c, r, v, formats[:int_right])
-                elseif j == 6 # p-values
+                elseif j in (5,6) # F and p-values
                     if v < 0.001
-                        LibXLSXWriter.worksheet_write_string(t, c, r, "< 0.001", formats[:pvalue])
+                        LibXLSXWriter.worksheet_write_string(t, c, r, "< 0.001", formats[:str_right])
                     elseif !ismissing(v)
                         LibXLSXWriter.worksheet_write_string(t, c, r, @sprintf("%.3f", v), formats[:str_right_b])
                     else
                         LibXLSXWriter.worksheet_write_string(t, c, r, "", formats[:str_right_b])
                     end
-                elseif ismissing(v)
-                    LibXLSXWriter.worksheet_write_string(t, c, r, "", formats[:str_right])
-                else
-                    LibXLSXWriter.worksheet_write_number(t, c, r, v, formats[:f_fmt])
+                # elseif ismissing(v)
+                #     LibXLSXWriter.worksheet_write_string(t, c, r, "", formats[:str_right])
+                # else
+                #     LibXLSXWriter.worksheet_write_number(t, c, r, v, formats[:f_fmt])
                 end
             end
             r += 1
